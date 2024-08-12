5f7eb5ee451ebd6e5f8bd92cbf7be455ae07fe16
SamanthAnnaiah <samanth.annaiah@gmail.com>
2024-08-12 22:32:21 +0530

    commit_5
    Fifth commit

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -3,51 +3,51 @@ import Google from "next-auth/providers/google";
 import { getuserid, putuser } from "./data-service";
 
 const authConfig = {
-    providers: [
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET
-        }
-        )
-    ],
-    callbacks: {
-        authorized({ auth, request }) {
-            return !!auth?.user;
-        },
-        async signIn({ user, account, profile, email, credentials }) {
-            if (user.email) {
-                let userid = await getuserid(user.email);
-                if (!userid) {
-                    try {
-                        let inserteddata = await putuser(user.name, user.email, user.image);
-                        if (inserteddata) {
-                            return true;
-                        }
-                        else {
-                            return false;
-                        }
-                    } catch (error) {
-                        return false;
-                    }
-                }
-                else {
-                    return true;
-                }
-            }
-            else {
-                return true;
-            }
-        },
-        async session({ session, user }) {
-            let userid = await getuserid(session.user.email);
-            session.user.userid = userid;
-            return session;
-        }
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
     },
-    pages: {
-        signIn: "/login"
-    }
+    async signIn({ user, account, profile, email, credentials }) {
+      if (user.email) {
+        let userid = await getuserid(user.email);
+        if (!userid) {
+          try {
+            let inserteddata = await putuser(user.name, user.email, user.image);
+            if (inserteddata) {
+              return true;
+            } else {
+              return false;
+            }
+          } catch (error) {
+            return false;
+          }
+        } else {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    },
+    async session({ session, user }) {
+      let userid = await getuserid(session.user.email);
+      session.user.userid = userid;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 };
 
-export const { auth, signIn, signOut, handlers: { GET, POST } } = NextAuth(authConfig);
-
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
